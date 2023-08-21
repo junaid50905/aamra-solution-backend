13ff769972ad7ec4c28c4550ae68798784f8c00c
junaid hossain <junaidhossain50905@gmail.com>
2023-08-21 17:12:37 +0600

    complete making user api except update profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,14 +4,106 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     // register
     public function register(Request $request)
     {
-        $register_user = User::create($request->all());
+        $request->validate([
+            'email' => 'required|unique:users,email',
+            'name' => 'required',
+            'password' => 'required'
+        ]);
 
-        return response()->json($register_user);
+        $user = User::create([
+            'email' => $request->email,
+            'name' => $request->name,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('register_' . $request->email)->plainTextToken;
+
+        return response([
+            'message' => 'Successfully registered',
+            'status' => 'success',
+            'token' => $token
+        ], 201);
     }
+
+    // login
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+
+            $token = $user->createToken('login_' . $request->email)->plainTextToken;
+
+            return response([
+                'message' => 'Successfully login',
+                'status' => 'success',
+                'token' => $token
+            ], 200);
+        }
+
+        return response([
+            'message' => 'Invalid credential',
+            'status' => 'failed'
+        ], 401);
+    }
+
+    // logout
+    public function logout()
+    {
+        if (auth()->check()) {
+
+            auth()->user()->tokens()->delete();
+
+            return response([
+                'message' => 'Successfully logged out',
+                'status' => 'success'
+            ], 200);
+        }
+
+        return response([
+            'message' => 'User not authenticated',
+            'status' => 'failed'
+        ], 401);
+    }
+
+    // logged user data
+    public function logged_user()
+    {
+        $logged_user = auth()->user();
+
+        return response([
+            'user' => $logged_user,
+            'status' => 'success'
+        ], 200);
+    }
+
+    // change password
+    public function change_password(Request $request)
+    {
+        $request->validate([
+            'password' => 'required|confirmed',
+        ]);
+
+        $logged_user = auth()->user();
+        $logged_user->password = Hash::make($request->password);
+        $logged_user->save();
+
+        return response([
+            'message' => 'Password changed successfully',
+            'status' => 'success'
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// public routes
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::get('/user', [UserController::class, 'logged_user']);
+    Route::post('/change-password', [UserController::class, 'change_password']);
 });
 
-Route::post('/register', [UserController::class, 'register']);
+
